7fc94c85068df691a03a9670ba8f9ad335e1b05a
juhisai <juhisai5566@gmail.com>
2023-01-23 12:50:34 +0530

    getting class from project

---
+++ src/test/java/Class1.java
@@ -0,0 +1,18 @@
+
+public class Class1 {
+	private void test1() {
+	System.out.println("Test");
+
+	}
+	private void test2() {
+
+		System.out.println("Test");
+
+	}
+	private void test3() {
+
+		System.out.println("Test");
+
+	}
+
+}
